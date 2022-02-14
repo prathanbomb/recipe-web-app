92c33f70f40d2f756477b118a6b9e6e976d82ae7
supitsara prathan <supitsara@digio.co.th>
2022-02-14 18:00:49 +0700

    fix api path

--- src/main/java/com/ss/assessment/controllers/RecipeController.java
+++ src/main/java/com/ss/assessment/controllers/RecipeController.java
@@ -25,7 +25,7 @@ import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api")
 public class RecipeController {
 
     @Autowired
